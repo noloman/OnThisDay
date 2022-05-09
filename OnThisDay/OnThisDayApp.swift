2b0d53228863cdd963ccdd1520210c689434c116
Manuel Lorenzo <manulorenzop@gmail.com>
2022-05-09 16:57:56 +0200

    Initial Commit

---
+++ OnThisDay/OnThisDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnThisDayApp.swift
+//  OnThisDay
+//
+//  Created by Manu on 09/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct OnThisDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
