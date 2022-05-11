1fc9502726bd91245f17b3ffd14a32539fd9fe8a
Manuel Lorenzo <manulorenzop@gmail.com>
2022-05-11 21:44:12 +0200

    add sorted table functionality

--- OnThisDay/Views/TableView.swift
+++ OnThisDay/Views/TableView.swift
@@ -8,16 +8,18 @@
 import SwiftUI
 
 struct TableView: View {
+    @State private var sortOrder = [KeyPathComparator(\Event.year)]
     var tableData: [Event]
+    var sortedTableData: [Event] {
+      return tableData.sorted(using: sortOrder)
+    }
     var body: some View {
-        Table(tableData) {
-            TableColumn("Year") {
+        Table(sortedTableData, sortOrder: $sortOrder) {
+            TableColumn("Year", value: \.year) {
                 Text($0.year)
             }
             .width(min: 50, ideal: 60, max: 100)
-            TableColumn("Title") {
-                Text($0.text)
-            }
+            TableColumn("Title", value: \.text)
         }
     }
 }
